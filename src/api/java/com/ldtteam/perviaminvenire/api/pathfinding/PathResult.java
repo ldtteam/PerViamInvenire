ef03a6a82a36a5428fa5b540458550e16d7e3b10
Marc Hermans <marc.hermans@ldtteam.com>
2022-02-01 17:37:09 +0100

    Remove the cancel with interrupt.

--- src/api/java/com/ldtteam/perviaminvenire/api/pathfinding/PathResult.java
+++ src/api/java/com/ldtteam/perviaminvenire/api/pathfinding/PathResult.java
@@ -230,7 +230,7 @@ public class PathResult<T extends Callable<Path>>
     {
         if (pathCalculation != null)
         {
-            pathCalculation.cancel(true);
+            pathCalculation.cancel(false);
             pathCalculation = null;
         }
 
